9d6f08f55c6fe9ab6284713d243ea603ed5ad0a2
CompileYouth <CompileYouth@163.com>
2015-11-03 14:21:11 +0800

    modify ContactsPanelContainer  css

--- assets/panel/ContactsPanelContainer.js
+++ assets/panel/ContactsPanelContainer.js
@@ -6,12 +6,25 @@ export default class ContactsPanelContainer extends React.Component {
 		super( props );
 	}
 
+	componentDidMount() {
+		this.$element = $( React.findDOMNode( this ) );
+		this._initNav();
+		this._initMain();
+	}
+
+	_initNav() {
+		this.$nav = $( "<ul class=panel-icon><li><span class='fa fa-users h3'></span></li><li class=active><span class='fa fa-star h3'></span></li><li class=spring></li></ul>" );
+		this.$element.append( this.$nav );
+	}
+
+	_initMain() {
+		this.$main = $( "<main class=panel-main></main>" );
+		this.$element.append( this.$main );
+	}
+
 	render() {
 		return (
-			<div id="ab-contacts">
-				<ContactsListPanel />
-				<ContactsCollectionPanel />
-			</div>
+			<div id="ab-contacts"></div>
 		);
 	}
 }
